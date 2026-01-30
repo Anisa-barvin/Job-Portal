74121ae25747af8ee1b6fbdcaea713f3f3b61855
Anisa-barvin <barvinanisa@gmail.com>
2026-01-30 19:00:34 +0530

    Protected Routes is added

--- job-portal-frontend/src/pages/routes/AppRoutes.jsx
+++ job-portal-frontend/src/pages/routes/AppRoutes.jsx
@@ -1,4 +1,4 @@
-import { Routes, Route } from "react-router-dom";
+import { BrowserRouter,Routes, Route } from "react-router-dom";
 import Home from "../Home";
 import Login from "../auth/Login";
 import Register from "../auth/Register";
@@ -8,18 +8,37 @@ import ApplyJob from "../user/ApplyJob";
 import JobDetails from "../recruiter/JobDetails";
 import ForgotPassword from "../auth/ForgotPassword";
 import ResetPassword from "../auth/ResetPassword"; 
+
+import ProtectedRoute from "../../components/ProtectedRoute";
+import Unauthorized from "../Unauthorized";
 function AppRoutes() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/login" element={<Login />} />
       <Route path="/register" element={<Register />} />
-      <Route path="/user/dashboard" element={<UserDashboard />} />
-      <Route path="/recruiter/dashboard" element={<RecruiterDashboard />} />
+          <Route path="/unauthorized" element={<Unauthorized />} />
+      <Route
+          path="/user/dashboard"
+          element={
+            <ProtectedRoute allowedRoles={["user"]}>
+              <UserDashboard />
+            </ProtectedRoute>
+          }
+        />
+        
+       <Route
+          path="/recruiter/dashboard"
+          element={
+            <ProtectedRoute allowedRoles={["recruiter"]}>
+              <RecruiterDashboard />
+            </ProtectedRoute>
+          }
+        />
       <Route path="/user/apply/:id" element={<ApplyJob />} />
       <Route path="/recruiter/jobs/:id" element={<JobDetails />} />
       <Route path="/forgot-password" element={<ForgotPassword />} />
-<Route path="/reset-password/:token" element={<ResetPassword />} />
+      <Route path="/reset-password/:token" element={<ResetPassword />} />
 
     </Routes>
   );
